f42b3566f472937981e3691ba73caa87db6a20d0
Quentin Garchery <garchery.quentin@gmail.com>
2022-06-29 14:11:35 +0200

    sanity rule

---
+++ certora/scripts/sanity.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+make -C certora munged
+
+certoraRun \
+    certora/munged/Token.sol \
+    --packages solmate=lib/solmate/src/ \
+    --verify Token:certora/specs/sanity.spec \
+    --rule sanity \
+    --loop_iter 2 \
+    --solc_args '["--optimize"]' \
+    --settings -t=60 \
+    --msg "sanity" \
+    --send_only \
+    $*
