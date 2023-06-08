851c6b57c3c0dbd66d4353721c9849a025596561
Quentin Garchery <garchery.quentin@gmail.com>
2023-06-08 15:44:55 +0200

    chore: migrate to CVL2

--- certora/scripts/authorizations.sh
+++ certora/scripts/authorizations.sh
@@ -5,7 +5,7 @@ make -C certora munged
 certoraRun \
     certora/munged/Token.sol \
     certora/helpers/Utils.sol \
-    --packages solmate=lib/solmate/src/ \
+    --packages solmate=lib/solmate/src \
     --verify Token:certora/specs/authorizations.spec \
     --optimistic_loop \
     --loop_iter 2 \
